ef38c5f66b259591ac4901527ff2c1836fa9872c
Kevin Baluyot <baluyotkevin33@yahoo.com>
2023-06-19 16:43:47 -0700

    reupgraded and remigrated

--- react-app/src/components/Posts/PostOnPostForm.js
+++ react-app/src/components/Posts/PostOnPostForm.js
@@ -21,7 +21,7 @@ const PostOnPostForm = ({ postId, post, formType }) => {
         if(title.length < 5) errors.title = "Please enter 5 characters or more"
         if(title.length > 30) errors.title = "You cannot exceed 30 characters"
         if(body.length < 10) errors.body = "Please enter 10 characters or more"
-        if(body.length > 455) errors.body = "You cannot exceed 455 characters"
+        if(body.length > 1000) errors.body = "You cannot exceed 1000 characters"
         // const check = dispatch(thunkOnePost(postId + 1))
         // if(check.children) errors.check = "You've already created a post"
         setValidationErrors(errors)
