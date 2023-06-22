ccc4745e4adae72e26b6ccf74b63080414eb62aa
Kevin Baluyot <baluyotkevin33@yahoo.com>
2023-06-22 15:42:42 -0700

    fixed createpostonpost

--- react-app/src/components/Posts/CreatePostOnPost.js
+++ react-app/src/components/Posts/CreatePostOnPost.js
@@ -4,8 +4,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { useEffect, Fragment } from "react";
 import { useHistory } from "react-router-dom";
 
-const CreatePostOnPost = () => {
-    const { postId } = useParams()
+const CreatePostOnPost = ({postId}) => {
+    // const { postId } = useParams()
 
     const post = {
         title: "",
