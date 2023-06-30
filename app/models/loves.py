8e2e1f766dcd91e1cbaff0e3ace59fc9b4042038
Kevin Baluyot <baluyotkevin33@yahoo.com>
2023-06-30 15:50:01 -0700

    changed column Name to moidel name

--- app/models/loves.py
+++ app/models/loves.py
@@ -3,6 +3,6 @@ from .db import db
 love = db.Table(
     'loves',
     db.Model.metadata,
-    db.Column("users", db.Integer, db.ForeignKey("users.id"), primary_key=True),
-    db.Column("posts", db.Integer, db.ForeignKey("posts.id"), primary_key=True)
+    db.Column("User", db.Integer, db.ForeignKey("users.id"), primary_key=True),
+    db.Column("Post", db.Integer, db.ForeignKey("posts.id"), primary_key=True)
 )
